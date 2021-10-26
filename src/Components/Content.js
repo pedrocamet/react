a66c455a61a837a5bd699646b42cd2e72458f7f4
fdeloredo <federicodeloredo@gmail.com>
2021-10-26 11:01:24 -0300

    correcion componentes

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./app5.css";
 import product from '../images/product.svg';
+import EtiquetaInicial from './EtiquetaInicial';
+import EtiquetaPrecio from './EtiquetaPrecio';
+import EtiquetaUsers from './EtiquetaUsers';
+import EtCategoria from './EtCategoria';
 
 function Content (){
   return (
@@ -14,54 +18,17 @@ function Content (){
 
 						
 						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1"> Products in Data Base</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">135</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
+							<EtiquetaInicial/>
 						</div>
 
 						
 						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Amount in products</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">$546.456</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
+							<EtiquetaPrecio/>
 						</div>
 
 						
 						<div className="col-md-4 mb-4">
-							<div className="card border-left-warning shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Users quantity
-											</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">38</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-user-check fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
+						<EtiquetaUsers/>
 						</div>
 					</div>
 
@@ -91,48 +58,14 @@ function Content (){
 								</div>
 								<div className="card-body">
 									<div className="row">
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 01
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 02
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 03
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 04
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 05
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-info text-white shadow">
-												<div className="card-body">
-													Category 06
-												</div>
-											</div>
-										</div>
+										
+										<EtCategoria/>
+										<EtCategoria/>
+										<EtCategoria/>
+										<EtCategoria/>
+										<EtCategoria/>
+										<EtCategoria/>
+										
 									</div>
 								</div>
 							</div>
---
+++ src/Components/EtCategoria.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./app5.css";
+
+
+function EtCategoria (){
+  return (
+<div className="col-lg-6 mb-4">
+											<div className="card bg-info text-white shadow">
+												<div className="card-body">
+													Category 01
+												</div>
+											</div>
+										</div>
+
+
+
+ )};
+  export default EtCategoria;
