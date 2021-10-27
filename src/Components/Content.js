88d445ddfadb39ae033899cf207af7fdd4519821
fdeloredo <federicodeloredo@gmail.com>
2021-10-27 11:54:45 -0300

    no me deja pullear sino

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -9,8 +9,7 @@ import MostrarProductos from './MostrarProductos';
 import UltimoAuto from './UltimoAuto';
 
 function Content (){
-  return (
-    <div>
+  return (    <div>
      
 					<div className="d-sm-flex align-items-center justify-content-between mb-4">
 						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
